706a469a62e119325d912f88ec8cc7a63f634fcf
Devrct-Front <raonbowcoffeepubg@gmail.com>
2024-03-20 06:26:33 +0330

    login

--- src/components/modules/Input.jsx
+++ src/components/modules/Input.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function Input() {
+export default function Input({ type, placeholder, icon: Icon }) {
   return (
-    <div>Input</div>
-  )
+    <div className="relative">
+      <input
+        type={type || "text"}
+        className="bg-[#f3f4f6] h-[3.25rem] w-full rounded-[10px] text-sm text-[#64748b]"
+        placeholder={placeholder}
+        style={{ paddingLeft: "2.35rem", paddingRight: "0.85rem" }}
+      />
+      {Icon && <Icon className="absolute top-0 bottom-0 left-3 my-auto text-[#64748b] text-[20px]" />}
+    </div>
+  );
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,85 +1,53 @@
 import React from "react";
-import "../css/ElementProprety/FormInput.css";
+// component
+import Input from "../components/modules/Input";
+
+// icon
+import { HiOutlineUser } from "react-icons/hi2";
+import { LuPhone } from "react-icons/lu";
+import { FaRegEnvelope } from "react-icons/fa";
+import { FiLock } from "react-icons/fi";
+
 
 export default function Login() {
   return (
     <>
-      <main className="flex justify-center items-center flex-col relative px-4 py-6 min-h-screen">
-        {/* logo sabzlearn  */}
-        <a
-          href="/"
-          className="flex items-center gap-x-3.5 mb-10"
-        >
+      <section className="flex justify-center items-center flex-col relative px-4 py-6 min-h-screen">
+        <a href="" className="flex items-center gap-x-3 sm:mb-10 mb-4">
           <img
             src="https://sabzlearn.ir/wp-content/themes/sabzlearn-theme/images/logo.webp"
+            alt="logo"
             className="h-[62px]"
-            alt="سبز لرن"
-            loading="lazy"
           />
-          <span className="w-[136px] h-[62px]">
-            <h1 className="font-danaDemibold text-[40px]">سبزلرن</h1>
-            <p>S a b z l e r n . i r</p>
-          </span>
+          <div className="w-[138px] h-[62px] sm:mb-3 sm:mt-0 mt-10">
+            <h2 className="sm:text-[30px] text-[20px] font-danaDemibold">سبزلرن</h2>
+            <h2 className="sm:text-[18px] text-[12px] font-danaLight">
+              S a b z l e a r n . i r
+            </h2>
+          </div>
         </a>
 
-        {/* page Body  */}
-        <div className="max-w-[330px] w-full pt-5 pb-6 px-6 text-center bg-white dark:bg-darker rounded-2xl">
-          <h4 className="font-danaDemibold text-xl mb-4 sm:mb-4.5">عضویت</h4>
-
+        <div className="max-w-[330px] w-full pt-5 pb-6 px-6 text-center bg-white rounded-2xl">
+          <h4 className="font-danaMedium text-xl mb-4 sm:mb-5">عضویت</h4>
           <p className="mb-5">
-            قبلا ثبت نام کرده اید؟
-            <a
-              href="/login"
-              className="font-danaDemiBold text-green-500"
-            >
+            قبلا ثبت نام کرده اید؟{" "}
+            <a href="" className="font-danaDemibold text-green-500 ">
               وارد شوید
             </a>
           </p>
 
-          <form className="user-data__form space-y-5">
-            <div className="relative">
-              <input
-                type="text"
-                className="user-data__username form-input has-icon"
-                placeholder="نام کاربری"
-              />
-            </div>
-            <div className="relative">
-              <input
-                type="text"
-                className="user-data__username form-input has-icon"
-                placeholder="شماره موبایل"
-              />
-            </div>
-            <div className="relative">
-              <input
-                type="email"
-                className="user-data__username form-input has-icon"
-                placeholder="آدرس ایمیل"
-              />
-            </div>
-            <div className="relative">
-              <input
-                type="password"
-                className="user-data__username form-input has-icon"
-                placeholder="رمز عبور"
-              />
-            </div>
-          </form>
-        </div>
-
-        {/* footer page */}
-        <div className="max-w-[330px] w-full mx-auto text-center mt-7 sm:mt-8 font-danaMedium">
-          با عضویت در سایت، تمامی قوانین و شرایط استفاده از خدمت
-          <a href="https://sabzlearn.ir" className="text-green-500">
-            سبزلرن
-          </a>{" "}
-          را پذیرفته اید.
+          <div className="form-data space-y-5">
+            <Input type="text" placeholder="نام کاربری" icon={HiOutlineUser} />
+            <Input type="text" placeholder="شماره موبایل" icon={LuPhone} />
+            <Input type="text" placeholder="ادرس ایمیل" icon={FaRegEnvelope} />
+            <Input type="text" placeholder="رمز عبور" icon={FiLock} />
+            <button className="bg-[#22c55e] text-white rounded-full text-[1rem] px-[1rem] h-[52px] gap-1 w-full">ادامه</button>
+          </div>
         </div>
 
         <div className="hidden lg:block absolute top-0 left-0 w-[300px] h-[300px] bg-sky-500 opacity-20 blur-[120px] rounded-full"></div>
         <div className="hidden lg:block absolute bottom-0 right-0 w-[300px] h-[300px] bg-amber-400 opacity-20 blur-[120px] rounded-full"></div>
-      </main>
+      </section>
     </>
   );
 }
