b6b9da668f3544bea4d304a81af87ac9094adbda
Devrct-Front <raonbowcoffeepubg@gmail.com>
2024-03-22 04:18:06 +0330

    update courseparts

--- src/components/modules/Course/CourseData/CourseHeadlines.jsx
+++ src/components/modules/Course/CourseData/CourseHeadlines.jsx
@@ -68,7 +68,7 @@ export default function CourseHeadlines({ data }) {
                             {episode.id}
                           </div>
                           <a
-                             href={`/lesson/${data.name}-${session.id}`}
+                             href={`/lesson/${data.name}-${session.id}:${episode.id}`}
                             className="inline-block lg:max-w-3/4 text-sm md:text-base group-hover:text-green-500"
                           >
                             {episode.name}
--- src/components/template/Course/CourseParts.jsx
+++ src/components/template/Course/CourseParts.jsx
@@ -1,20 +1,68 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCourses } from "../../../services/Redux/actions";
+import Breadcrumb from '../../modules/Categoreis/Breadcrumb';
+import { LuHome } from "react-icons/lu";
 
 export default function CourseParts() {
-  const param = useParams()
-  useEffect(()=>{
-    console.log(param);
-  },[])
+  const [course , setCourse]=useState([])
+  const [episodeCourse , setEpisodeCourse] = useState([])
+  const { courseInfo } = useParams();
+  const dispatch = useDispatch();
+  const dataCourses = useSelector((state) => state.courses);
+  
+  useEffect(() => {
+    dispatch(fetchCourses());
+  }, [dispatch]);
+
+  useEffect(() => {
+     const foundCourse = dataCourses.find(c => c.name === courseInfo.split('-')[0]);
+      if (foundCourse) {
+        setCourse(foundCourse)
+        const chapterId = parseInt(courseInfo.split('-')[1]);
+        const foundChapter = foundCourse.session.find(chap => chap.id === chapterId);
+        console.log(foundChapter);
+        if (foundChapter) {
+          const episodeId = parseInt(courseInfo.split(':')[1])
+          const foundEpisode = foundChapter.Episode.find(epsid => epsid.id === episodeId)
+          setEpisodeCourse(foundEpisode);
+        }
+      }
+  }, [dataCourses, courseInfo]);
+
   return (
     <section className='mx-auto overflow-x-hidden mt-8 sm:mt-10'>
       <div className='container'>
         {/* <!-- Breadcrumb --> */}
-        <div></div>
+       <Breadcrumb
+          links={[
+            {
+              id: 1,
+              title: <LuHome className="text-[25px] text-gray-400 ml-2" />,
+              to: "/",
+            },
+            {
+              id: 2,
+              title: "دوره ها",
+              to: "/courses",
+            },
+            {
+              id: 3,
+              title: `${course.category}`,
+              to: `/category/${course.category}`,
+            },
+            {
+              id: 4,
+              title: `${course.title}`,
+              to: `/course/${course.name}`,
+            },
+          ]}
+        />
 
         {/* <!-- Course Lesson --> */}
         <div className='aspect-video mt-8 sm:mt-10 overflow-hidden rounded-xl'>
-          <video src="https://tech.sabzlearn.ir/uploads/ce01010101it/next/Next001-intro.mp4?h=6Q8udC2_lp0jWI0xokhYzQ&t=1711118380" className='w-full m-auto overflow-hidden rounded-[20px]' controls></video>
+            <video src={episodeCourse.video} className='w-full m-auto overflow-hidden rounded-[20px]' controls></video>
         </div>
 
         <div className='grid grid-cols-12 gap-y-6 gap-x-5 lg:gap-x-7 mt-6 lg:mt-8 '>
@@ -26,5 +74,5 @@ export default function CourseParts() {
 
       </div>
     </section>
-  )
+  );
 }
